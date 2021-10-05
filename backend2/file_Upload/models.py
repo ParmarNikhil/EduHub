f3bdc0e958a5df90080ad4d4c03e6183745637c9
ParmarNikhil <np0598029@gmail.com>
2021-10-05 21:07:57 +0530

    used cookies to store the authentication token of user

--- backend2/file_Upload/models.py
+++ backend2/file_Upload/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class FileUpload(models.Model):
     title = models.CharField(max_length=100)
     file = models.FileField()
+    
     # data = models.CharField(max_length=100000, default="")
     
 
